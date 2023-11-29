ecaa1cdc0af743e70b53f6bee920a14d2f5b08de
Kristofer <kristofer@fmckl.se>
2023-11-29 09:09:56 +0100

    List sessions as pills

--- src/react_demo_frontend/src/components/ActiveSessions.tsx
+++ src/react_demo_frontend/src/components/ActiveSessions.tsx
@@ -5,6 +5,7 @@ import { Session } from "../ic/session.type";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { react_demo_backend } from "../../../declarations/react_demo_backend";
 import { useSession } from "../ic/useSession";
+import AddressPill from "./AddressPill";
 
 export default function ActiveSessions() {
   const [sessions, setSessions] = useState<Session[]>([]);
@@ -28,18 +29,16 @@ export default function ActiveSessions() {
 
   return (
     <div className="w-full max-w-2xl border-zinc-700/50 border-[1px] bg-zinc-900 px-5 drop-shadow-xl rounded-3xl flex flex-col items-center">
-      <div className="flex flex-col items-center w-full p-8 gap-10">
-        <div className="text-4xl font-bold">Active Sessions</div>
+      <div className="flex flex-col items-center w-full gap-10 p-8">
+        <div className="text-2xl font-bold">Active Sessions</div>
         <div className="flex flex-col items-center gap-5">
-          <div className="flex items-center text-xl font-bold gap-2">
+          <div className="flex flex-wrap items-center justify-center gap-2">
             {loading && (
               <FontAwesomeIcon className="w-4 h-4" icon={faCircleNotch} spin />
             )}
-            <pre className="text-zinc-400">
-              {sessions.map((session) => (
-                <div key={session.address}>{session.address}</div>
-              ))}
-            </pre>
+            {sessions.map((session) => (
+              <AddressPill address={session.address} key={session.address} />
+            ))}
           </div>
         </div>
       </div>
