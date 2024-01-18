6a70877f95a675e7d1c70b2f6c258a20f80c9862
Kristofer <kristofer@fmckl.se>
2024-01-18 12:10:24 +0100

    Refactor error handling in Actors component

--- src/frontend/src/ic/Actors.tsx
+++ src/frontend/src/ic/Actors.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable react-refresh/only-export-components */
 import {
   ActorProvider,
+  InterceptorErrorData,
   InterceptorRequestData,
   createActorContext,
   createUseActorHook,
+  isIdentityExpiredError,
 } from "ic-use-actor";
 import { canisterId, idlFactory } from "../../../declarations/backend/index";
 
 import { ReactNode } from "react";
 import { _SERVICE } from "../../../declarations/backend/backend.did";
-import { isDelegationValid } from "@dfinity/identity";
 import toast from "react-hot-toast";
 import { useSiweIdentity } from "ic-use-siwe-identity";
 
@@ -17,7 +18,7 @@ const actorContext = createActorContext<_SERVICE>();
 export const useActor = createUseActorHook<_SERVICE>(actorContext);
 
 export default function Actors({ children }: { children: ReactNode }) {
-  const { identity, delegationChain, clear } = useSiweIdentity();
+  const { identity, clear } = useSiweIdentity();
 
   const errorToast = (error: unknown) => {
     if (typeof error === "object" && error !== null && "message" in error) {
@@ -27,42 +28,22 @@ export default function Actors({ children }: { children: ReactNode }) {
     }
   };
 
-  const handleResponseError = (error: unknown) => {
-    console.error("onResponseError", error);
-
-    // This could be done using a library like Zod, but I don't want to add a dependency for this one check.
-    if (
-      (delegationChain && !isDelegationValid(delegationChain)) || // Delegation (most likely) expired
-      (typeof error === "object" &&
-        error !== null &&
-        "error" in error &&
-        typeof error.error === "object" &&
-        error.error !== null &&
-        "name" in error.error &&
-        error.error.name === "AgentHTTPResponseError" &&
-        "message" in error.error &&
-        typeof error.error.message === "string" &&
-        error.error.message.includes(
-          "Invalid delegation: Invalid canister signature"
-        ) &&
-        "response" in error.error &&
-        error.error.response !== null &&
-        typeof error.error.response === "object" &&
-        "status" in error.error.response &&
-        error.error.response.status === 403) // Some other issue with the delegation
-    ) {
-      toast.error("Invalid Identity", {
-        id: "invalid-identity",
+  const handleResponseError = (data: InterceptorErrorData) => {
+    console.error("onResponseError", data.error);
+    if (isIdentityExpiredError(data.error)) {
+      toast.error("Login expired.", {
+        id: "login-expired",
         position: "bottom-right",
       });
       setTimeout(() => {
         clear(); // Clears the identity from the state and local storage. Effectively "logs the user out".
+        window.location.reload(); // Reload the page to reset the UI.
       }, 1000);
       return;
     }
 
-    if (typeof error === "object" && error !== null && "message" in error) {
-      errorToast(error);
+    if (typeof data === "object" && data !== null && "message" in data) {
+      errorToast(data);
     }
   };
 
